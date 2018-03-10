1c5b43ddc703ca4ac67d44b802032818f94dd01c
rwagget <ryanwagget@gmail.com>
2018-03-09 21:00:10 -0500

    Populating Movies

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -21,54 +21,5 @@
     });
   }
 
-  function deleteRecord()
-  {
-    let url = "api/" + this.id;
-
-    fetch(url, {method : 'delete'})
-    .then((resp) => resp.json())
-    .then((data) =>
-    {
-      console.log(data);
-    })
-    .catch(function(error)
-    {
-      console.log(error);
-    });
-  }
-
-  function insertRecord()
-  {
-    let url = "api/" + this.id;
-
-    fetch(url,
-      {
-        method : 'post',
-        header : {
-          'Accept' : 'application/json, text-plain, */*',
-          'Content-type' : 'application/json'
-        },
-        body: JSON.stringify({
-          model : "F56",
-          modelName : "Mini Cooper F56",
-          pricing : "22, 190",
-          modelDetails : "Lots and lots of text",
-          imagePath : "F56.jpg"
-        })
-      })
-    .then((resp) => resp.json())
-    .then((data) =>
-    {
-      console.log(data);
-    })
-    .catch(function(error)
-    {
-      console.log(error);
-    });
-  }
-
-  //event handling always goes at the bottom
   getButtons.forEach(button => button.addEventListener('click', fetchData));
-  //deleteButton.addEventListener('click', deleteRecord);
-  //postButton.addEventListener('click', insertRecord);
 })();
--- routes/details.js
+++ routes/details.js
@@ -3,17 +3,15 @@ var connect = require('../utils/sqlConnect');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  connect.query('SELECT * FROM mainmodel', (err, result) => {
+router.get('/:id', (req, res) => {
+  connect.query(`SELECT * FROM tbl_movies WHERE movies_id = ${req.params.id}`, (err, result) => {
     if(err) {
       throw err; console.log(err);
     } else {
       console.log(result);
 
       res.render('details', {
-        title: 'Mini Cars',
-        message : "A Selection of minis",
-        carData : result //will give array that we can loop through
+        moviesData : result[0] //will give array that we can loop through
       });
     }
   });
